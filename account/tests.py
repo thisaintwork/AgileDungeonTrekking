8545ca32ec9fb17a2e15bb86caaf4bc7bf35d845
dliu936 <54752285+dliu936@users.noreply.github.com>
2022-11-10 13:35:21 -0500

    Add files via upload
    Built and committed unit tests; Each pass

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from django.contrib.auth import get_user_model
+from django.test import TestCase,Client
+from django.urls import reverse
 
-# Create your tests here.
+#Unit test of creating a user
+class CustomUserTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='Bill',
+            email='Bill@email.com',
+            password='testpass123'
+        )
+        self.assertEqual(user.username, 'Bill')
+        self.assertEqual(user.email, 'Bill@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+
+#Unit test of the Registration page
+
+class RegisterPageTests(TestCase):
+    # see https://docs.djangoproject.com/en/4.1/topics/testing/tools/
+    def setup(self):
+        self.client  = Client()
+
+    def test_register_template(self):
+        response = self.client.get('/register/')
+        self.assertEqual(response.status_code, 200)
+
+#Unit test of the Login page
+class LoginPageTests(TestCase):
+    def setup(self):
+        self.client  = Client()
+
+    def test_register_template(self):
+        response = self.client.get('/login/')
+        self.assertEqual(response.status_code, 200)
