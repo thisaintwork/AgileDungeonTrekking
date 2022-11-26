3e31b98ad63bfc33b04982986dd712886811cc56
dhwcode <112971453+dhwcode@users.noreply.github.com>
2022-11-26 03:12:30 -0500

    Update settings.py

--- adt/settings.py
+++ adt/settings.py
@@ -16,7 +16,7 @@ import sys
 import dj_database_url
 from django.core.management.utils import get_random_secret_key
 
-
+# test
 # test
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
