0dc9e74cbcee79120d87d15f11bababd816d8392
dliu936 <54752285+dliu936@users.noreply.github.com>
2022-12-08 19:05:44 -0500

    Update urls.py

--- adt/urls.py
+++ adt/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('creatures/', include('creatures.urls'), name='creatures'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL,
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
                           document_root=settings.MEDIA_ROOT)
-urlpatterns += staticfiles_urlpatterns()
+    urlpatterns += staticfiles_urlpatterns()
