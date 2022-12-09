053b954bcf952806d4f835436d0f86cc1d05296d
dliu936 <54752285+dliu936@users.noreply.github.com>
2022-12-08 19:06:59 -0500

    Update urls.py

--- characters/urls.py
+++ characters/urls.py
@@ -12,4 +12,5 @@ urlpatterns = [
     path('modify/<int:id>', views.character_modify, name='character_modify'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
