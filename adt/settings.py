5dd35a5538d3b2900e9f9abee1c0f29548958f0d
dl <dil936@g.harvard.edu>
2022-12-09 22:19:17 -0500

    Moved media to static_files

--- adt/settings.py
+++ adt/settings.py
@@ -157,8 +157,8 @@ if DEBUG:
 else:
     STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/static_files/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static_files')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
